d3c41d077a0f009e44da0cb4ed6cc89f1287bb8f
jarcia727 <104233320+jarcia727@users.noreply.github.com>
2025-02-04 01:18:52 -0500

    actualizacion de  app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
 
+        function agregarAmigo() {
+            let input = document.getElementById("nombreAmigo");
+            let nombre = input.value.trim();
+            
+            if (nombre === "") {
+                alert("Por favor, inserte un nombre.");
+                return;
+            }
+            
+            amigos.push(nombre);
+            actualizarLista();
+            input.value = "";
+        }
+
+        function actualizarLista() {
+            let lista = document.getElementById("listaAmigos");
+            lista.innerHTML = "";
+            amigos.forEach(nombre => {
+                let li = document.createElement("li");
+                li.textContent = nombre;
+                lista.appendChild(li);
+            });
+        }
